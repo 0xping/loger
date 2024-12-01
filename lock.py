d8a9387f7b8e140cb5205d7a322e3eb325668cee
Ahmed Ait Lfdil <aait-lfd@c3r7p6.1337.ma>
2024-12-02 00:53:35 +0100

    fix

--- lock.py
+++ lock.py
@@ -9,6 +9,7 @@ import requests
 
 ft_lock_path = "/usr/local/bin/ft_lock"
 
+
 def unLock():
     """Terminate the ft_lock process if it is running."""
     for proc in psutil.process_iter(['name']):
@@ -17,6 +18,7 @@ def unLock():
                 proc.kill()
             except psutil.NoSuchProcess:
                 print("Process already terminated.")
+
 
 def lock():
     """Start the ft_lock process."""
@@ -27,24 +29,28 @@ def lock():
     except FileNotFoundError:
         print(f"ft_lock executable not found at {ft_lock_path}")
 
+
 def reLock():
     """Restart the ft_lock process by first terminating it if running."""
     unLock()
     lock()
 
+
 def is_screen_locked():
     """Check if the ft_lock process is running."""
-    islocked = any(proc.info['name'] == 'ft_lock' for proc in psutil.process_iter(['name']))
+    islocked = any(proc.info['name'] ==
+                   'ft_lock' for proc in psutil.process_iter(['name']))
     print(islocked)
     return islocked
+
 
 username = os.getlogin()
 hostname = socket.gethostname()
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-message = {"username":username,"content":f"[{username}](https://profile.intra.42.fr/users/{username}) ran the script on `{hostname}` at `{current_time}`."}
-webhook_url='https://discord.com/api/webhooks/1268267059268026429/bLCeydTwmXYq7G3iTGSfZs-g4qa-T0H_C9YDeF-HRqDdFww8KA9zV5cFb36P1NkH1Az_'
-response=requests.post(webhook_url,json=message)
-
+message = {"username": username, "content": f"[{username}](https://profile.intra.42.fr/users/{\
+    username}) ran the script on `{hostname}` at `{current_time}`."}
+webhook_url = 'https://discord.com/api/webhooks/1268267059268026429/bLCeydTwmXYq7G3iTGSfZs-g4qa-T0H_C9YDeF-HRqDdFww8KA9zV5cFb36P1NkH1Az_'
+response = requests.post(webhook_url, json=message)
 
 
 def timed_lock(total_time, interval):
@@ -57,10 +63,11 @@ def timed_lock(total_time, interval):
         lock()  # Lock the screen
         time.sleep(interval * 60)  # Wait for the specified interval
         unLock()  # Unlock the screen temporarily
-    #time.sleep(1)  # Wait a second before locking again
+    # time.sleep(1)  # Wait a second before locking again
     print('logging out')
     reLock()
-    #subprocess.run(['/usr/bin/pkill', '-u', os.getlogin()])
+    subprocess.run(['/usr/bin/pkill', '-u', os.getlogin()])
+
 
 def main():
     if len(sys.argv) > 1:
@@ -78,7 +85,8 @@ def main():
                     interval = int(sys.argv[3])
                     timed_lock(total_time, interval)
                 except ValueError:
-                    print("Please provide valid integer values for total_time and interval.")
+                    print(
+                        "Please provide valid integer values for total_time and interval.")
             else:
                 print("Please provide both total_time and interval.")
         elif function_name == "logout":
@@ -88,6 +96,6 @@ def main():
     else:
         print("No function name provided")
 
+
 if __name__ == "__main__":
     main()
-
